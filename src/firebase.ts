4d1910bac340c408131641a2d45a630a0631e311
hwan20c <hwan20c@gmail.com>
2024-02-29 15:15:34 +0900

    firebase update

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC31n4Vpw8umVbht6EeRVcDak_fvRD-MiQ",
-  authDomain: "nwitter-reloaded-2f6c1.firebaseapp.com",
-  projectId: "nwitter-reloaded-2f6c1",
-  storageBucket: "nwitter-reloaded-2f6c1.appspot.com",
-  messagingSenderId: "195282384292",
-  appId: "1:195282384292:web:26e9f9fc2bdcd0b8787d31",
+  apiKey: "AIzaSyB69uumG3h9z9IbbhVa-Ui-6dr4KZ-Z-Gg",
+  authDomain: "nwitter-reroaded.firebaseapp.com",
+  projectId: "nwitter-reroaded",
+  storageBucket: "nwitter-reroaded.appspot.com",
+  messagingSenderId: "959397877734",
+  appId: "1:959397877734:web:df671a8789a1ca81042827",
 };
 
 const app = initializeApp(firebaseConfig);
