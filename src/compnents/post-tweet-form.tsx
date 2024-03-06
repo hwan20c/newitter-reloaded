a4ba0bb2c37d5916f374e581bfc8cbb70e23da16
hwan20c <hwan20c@gmail.com>
2024-03-06 15:14:52 +0900

    update a file upload

--- src/compnents/post-tweet-form.tsx
+++ src/compnents/post-tweet-form.tsx
@@ -1,5 +1,8 @@
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
 import styled from "styled-components";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Form = styled.form`
   display: flex;
@@ -64,13 +67,53 @@ export default function PostTweetForm() {
     setTweet(e.target.value);
   };
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { files } = e?.target;
+    const { files } = e.target;
+
     if (files && files.length === 1) {
-      setFile(files[0]);
+      const selectedFile = files[0];
+      const fileSizeLimit = 1 * 1024 * 1024; // 1MB
+
+      if (selectedFile.size <= fileSizeLimit) {
+        setFile(selectedFile);
+      } else {
+        alert("파일 크기는 1MB 이하여야 합니다.");
+      }
+    }
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    if (!user || isLoading || tweet === "" || tweet.length > 180) return;
+
+    try {
+      setLoading(true);
+      const doc = await addDoc(collection(db, "tweets"), {
+        tweet,
+        createdAt: Date.now(),
+        username: user.displayName || "Anonymous",
+        userId: user.uid,
+      });
+      if (file) {
+        const locationRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${doc.id}`
+        );
+        const result = await uploadBytes(locationRef, file);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(doc, {
+          photo: url,
+        });
+      }
+      setTweet("");
+      setFile(null);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
     }
   };
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <TextArea
         rows={5}
         maxLength={180}
