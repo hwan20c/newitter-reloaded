f3d626fc2d4f924399cc63be20d969060735abab
hwan20c <hwan20c@gmail.com>
2024-03-06 16:01:30 +0900

    update : show a post

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 import PostTweetForm from "../compnents/post-tweet-form";
+import Timeline from "../compnents/timeline";
 
-const Wraaper = styled.div``;
+const Wraaper = styled.div`
+  display: grid;
+  gap: 50px;
+  overflow-y: scroll;
+  grid-template-rows: 1fr 5fr;
+`;
 
 export default function Home() {
   return (
     <Wraaper>
       <PostTweetForm />
+      <Timeline />
     </Wraaper>
   );
 }
